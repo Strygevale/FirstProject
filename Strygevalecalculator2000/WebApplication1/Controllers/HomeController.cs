1ac0763a691d458449515cc4adec4ccee07abda8
Helsker <helsker777@gmail.com>
2018-07-12 12:29:20 +0600

    Helsker
    Add Webapplication form.

---
+++ Strygevalecalculator2000/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System.Web.Mvc;
+using Strygevalecalculator2000.OneArgument;
+using Calculator.TwoArguments;
+using ITwoArgumentsCalculator = Strygevalecalculator2000.ITwoArgumentsCalculator;
+using SelectListItem = System.Web.WebPages.Html.SelectListItem;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "multiply", Text ="multiply"
+                },
+                new SelectListItem()
+                {
+                    Value = "substraction", Text ="substraction"
+                },
+                new SelectListItem()
+                {
+                    Value = "minus", Text ="minus"
+                },
+                new SelectListItem()
+                {
+                    Value = "divide", Text ="divide"
+                }
+            };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double firstNumber, double secondNumber, string operation)
+        {
+            Calculator.TwoArguments.ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator(operation);
+            double result = calculator.Calculate(firstNumber, secondNumber);
+            ViewBag.Result = result;
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "multiply", Text ="multiply"
+                },
+                new SelectListItem()
+                {
+                    Value = "addition", Text ="addition"
+                },
+                new SelectListItem()
+                {
+                    Value = "substraction", Text ="substraction"
+                },
+                new SelectListItem()
+                {
+                    Value = "divide", Text ="divide"
+                }
+            };
+            return View();
+        }
+    }
+}
